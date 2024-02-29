92d88ccbc28354f2cfcaaedba0b50aaee00da168
liehalford <107470385+liehalford@users.noreply.github.com>
2024-02-29 14:33:20 +0100

    adding api to js

--- source/index.js
+++ source/index.js
@@ -1,8 +1,16 @@
+function searchCity(city) {
+  let apiKey = "ef28bo275ed9fd6ce37aba445ad5t8b0";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query={query}&key={key}";
+}
+
 function makeSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
+
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
