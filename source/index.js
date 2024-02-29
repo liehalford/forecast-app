c80b948dd8e2f8eeeed36030845838bca60155ca
liehalford <107470385+liehalford@users.noreply.github.com>
2024-02-29 14:22:03 +0100

    creating js file

---
+++ source/index.js
@@ -0,0 +1,9 @@
+function makeSearch(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", makeSearch);
