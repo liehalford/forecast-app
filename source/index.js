ceb5beff4b13d47e380888192ba615b7c3024212
liehalford <107470385+liehalford@users.noreply.github.com>
2024-02-29 15:51:33 +0100

    adding more weather index

--- source/index.js
+++ source/index.js
@@ -3,8 +3,40 @@ function refreshWeather(response) {
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
   let city = response.data.city;
+  let descriptionElement = document.querySelector(".description");
+  let description = response.data.condition.description;
+  let humidityElement = document.querySelector("#current-humidity");
+  let humidity = `${response.data.temperature.humidity}%`;
+  let windElement = document.querySelector("#wind-speed");
+  let windSpeed = `${response.data.wind.speed} km/h`;
+  let timeElement = document.querySelector(".current-time");
+  let date = new Date(response.data.time * 1000);
   cityElement.innerHTML = city;
+  descriptionElement.innerHTML = description;
+  humidityElement.innerHTML = humidity;
+  windElement.innerHTML = windSpeed;
+  timeElement.innerHTML = formatDate(date);
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0 ${minutes}`;
+  }
+  return ` ${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
