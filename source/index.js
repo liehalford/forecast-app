7c9f95ba6b568037a2298d8bc3568a7f2641c183
liehalford <107470385+liehalford@users.noreply.github.com>
2024-02-29 15:02:25 +0100

    adding search engine

--- source/index.js
+++ source/index.js
@@ -1,15 +1,22 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#current-temperature-id");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let city = response.data.city;
+  cityElement.innerHTML = city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
 function searchCity(city) {
   let apiKey = "ef28bo275ed9fd6ce37aba445ad5t8b0";
-  let apiUrl =
-    "https://api.shecodes.io/weather/v1/current?query={query}&key={key}";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function makeSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
 
-  cityElement.innerHTML = searchInput.value;
   searchCity(searchInput.value);
 }
 
