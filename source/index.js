fb6e4f99177f45464da5a65c5aadf99ffe5297e2
liehalford <107470385+liehalford@users.noreply.github.com>
2024-03-02 13:24:43 +0100

    Update index.js

--- source/index.js
+++ source/index.js
@@ -69,7 +69,6 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
   let forecastHtml = "";
   response.data.daily.forEach(function (day, index) {
     if (index < 5)
